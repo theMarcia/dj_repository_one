55ab7f7f702186d04cb4dfe53199a6ae0af5f5f1
Stok <972853939@qq.com>
2019-07-13 11:26:10 +0800

    二次提交

---
+++ parenttt/src/main/java/Jsdjfksd.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Jsdjfksd {
+
+    public static void main(String[] args) {
+//        Integer[] itr = {1, 12, 3, 135, 324};
+//        for (int i = 0; i < itr.length - 1; i++) {
+//            for (int j = 0; j < itr.length - 1 - i; j++) {
+//                if (itr[j] < itr[j + 1]) {
+//                    Integer temp = itr[j];
+//                    itr[j] = itr[j + 1];
+//                    itr[j + 1] = temp;
+//                }
+//            }
+//        }
+//        for (Integer integer : itr) {
+//            System.out.println(integer);
+//        }
+
+        new HashMap<Integer, String>().put(12,"sdf");
+
+
+        Set<Integer> str = new HashSet<>();
+        str.add(13);
+        str.add(13);
+        str.add(3);
+        str.add(6);
+        str.add(78);
+        str.forEach(v ->{
+            System.out.println(
+                    v
+            );
+        });
+
+    }
+
+
+}
